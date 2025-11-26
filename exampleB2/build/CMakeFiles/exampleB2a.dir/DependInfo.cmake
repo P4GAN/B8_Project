b65628f796bfb410daa957e8c9584a2c01d361f5
P4GAN <parkinpham@gmail.com>
2025-11-26 14:51:43 +0000

    first commit

---
+++ exampleB2/build/CMakeFiles/exampleB2a.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/parkinpham/Programming/Physics/B8_04/exampleB2a.cc" "CMakeFiles/exampleB2a.dir/exampleB2a.cc.o" "gcc" "CMakeFiles/exampleB2a.dir/exampleB2a.cc.o.d"
+  "/Users/parkinpham/Programming/Physics/B8_04/src/ActionInitialization.cc" "CMakeFiles/exampleB2a.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/exampleB2a.dir/src/ActionInitialization.cc.o.d"
+  "/Users/parkinpham/Programming/Physics/B8_04/src/DetectorConstruction.cc" "CMakeFiles/exampleB2a.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleB2a.dir/src/DetectorConstruction.cc.o.d"
+  "/Users/parkinpham/Programming/Physics/B8_04/src/DetectorMessenger.cc" "CMakeFiles/exampleB2a.dir/src/DetectorMessenger.cc.o" "gcc" "CMakeFiles/exampleB2a.dir/src/DetectorMessenger.cc.o.d"
+  "/Users/parkinpham/Programming/Physics/B8_04/src/EventAction.cc" "CMakeFiles/exampleB2a.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleB2a.dir/src/EventAction.cc.o.d"
+  "/Users/parkinpham/Programming/Physics/B8_04/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleB2a.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleB2a.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/Users/parkinpham/Programming/Physics/B8_04/src/RunAction.cc" "CMakeFiles/exampleB2a.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/exampleB2a.dir/src/RunAction.cc.o.d"
+  "/Users/parkinpham/Programming/Physics/B8_04/src/TrackerHit.cc" "CMakeFiles/exampleB2a.dir/src/TrackerHit.cc.o" "gcc" "CMakeFiles/exampleB2a.dir/src/TrackerHit.cc.o.d"
+  "/Users/parkinpham/Programming/Physics/B8_04/src/TrackerSD.cc" "CMakeFiles/exampleB2a.dir/src/TrackerSD.cc.o" "gcc" "CMakeFiles/exampleB2a.dir/src/TrackerSD.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
